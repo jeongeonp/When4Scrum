51a160c481f49d27b090ce7c17d23b9b70555170
ChoHnA <zzznd3@naver.com>
2019-01-21 17:06:57 +0900

    login2

--- src/components/LoginTab.js
+++ src/components/LoginTab.js
@@ -13,60 +13,48 @@ class LoginTab extends Component {
     }
   }
 
-  responseGoogle = (res) => {
-    this.setState({
-      id: res.googleId,
-      name: res.profileObj.name,
-      provider: 'google'
-    });
-
-    axios.post('/signin', {
-      id: this.state.id,
-      name: this.state.name
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  responseFail = (err) => {
-    console.error(err);
-  }
-
   render() {
+
+    const responseGoogle = (res) => {
+      this.setState({
+        id: res.googleId,
+        name: res.profileObj.name,
+        provider: 'google'
+      });
+  
+      axios.post('/signin', {
+        id: this.state.id,
+        name: this.state.name
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  
+    const responseFail = (err) => {
+      console.error(err);
+    }
+
     return (
       <main className="login-template">
         <div className="vCenterItems">
-          <h1>LOGIN WITH GOOGLE</h1>
-      
-        <GoogleLogin
-          clientId="966186460976-7p410jpe034ffbstn4g0kgq7s7il4492.apps.googleusercontent.com"
-          buttonText="GOOGLE"
-          onSuccess={responseGoogle}
-          onFailure={responseFail}
-        />
+          LOGIN WITH {" "}
+          
+          <GoogleLogin
+            clientId="966186460976-7p410jpe034ffbstn4g0kgq7s7il4492.apps.googleusercontent.com"
+            buttonText="GOOGLE"
+            onSuccess={responseGoogle}
+            onFailure={responseFail}
+          />
 
         </div>
+        <div>User: {this.state.name}</div>
       </main>
     );
   }
 }
-
-/*
-const LoginTab = ({form, children}) => {
-  return (
-    <main className="login-template">
-      <div className="vCenterItems">
-        For Login
-        <button>Login</button>
-      </div>
-      
-    </main>
-  );
-};
-*/
 
 export default LoginTab;
