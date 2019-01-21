930dd2600ba75a5be0f2c1388e080152defcd894
Jeongeon Park <patricia021912@gmail.com>
2019-01-21 21:54:22 +0900

    tables done, time select to go

--- src/components/SelectTable.js
+++ src/components/SelectTable.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import './SelectTable.css';
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+
 
 class SelectTable extends Component {
+    
+
     render() {
+        const options = ["0:00", "1:00", "2:00", "3:00"]
+        const defaultOption = options[0]
         return (
             <main className="table-template">
-              <div>
-                  From 
+              <div className="side-align">
+                  <div className="align">From</div>
+                  <Dropdown options={options} onChange={this._onselect} value={defaultOption} placeholder="select an option" className="side-paddings"></Dropdown>
               </div>
               <div>
-                  to
+                    <div className="align">to</div>
+                  <Dropdown options={options} onChange={this._onselect} value={defaultOption} placeholder="select an option" className="side-paddings"></Dropdown>
               </div>
             </main>
           );
--- src/components/TimeList.js
+++ src/components/TimeList.js
@@ -6,8 +6,11 @@ class TimeList extends Component {
 
 
   render() {
+    var candi_date = [0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    var first = candi_date.indexOf(1)/2;
+    var last = candi_date.lastIndexOf(1)/2;
     var rows = [];
-    for (var i=0; i<10; i++) {
+    for (var i=first; i<last; i++) {
       rows.push(<Time key={i} text={i + ":00"}/>)
       rows.push(<Time key={i} text={i + ":30"}/>)
     }
