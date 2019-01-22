2da926e37d8bb5a3c5202a454c5deee0e3c1af4b
euicheon <no1eclee@gmail.com>
2019-01-22 18:04:38 +0900

    commit

--- src/components/TimeList2.js
+++ src/components/TimeList2.js
@@ -3,37 +3,57 @@ import Time from './Time2';
 import axios from 'axios';
 
 var scurmId = "";
-
+var candi_date = [];
+var received = [];
+var rows = [];
 class TimeList2 extends Component {
-  
-    componentDidMount(){
-        axios.get("http://143.248.140.106:2980/scrum/"+"5c45d5fc2e4d2623227ffd10")
-        .then(res=>{
-            console.log('asd: ${res.scrum.candi_date}')
-        })
+    
+    state = {
+        isLoading: true,
     }
+    componentWillMount(){
+        axios.get("http://143.248.140.106:2980/scrum/5c45dbd52e4d2623227ffd11")
+        .then(res=>{
+            console.log('HI')
+            console.log(res.data.scrum.candi_date[0].time)
+            console.log(res.data.statistics)
+            candi_date = res.data.scrum.candi_date[0].time;
+            received = res.data.statistics
+            this.setState({
+                isLoading:false
+            });
+        })
+      }
 
     render() {
+        // candi_date = this.props.candi_date;
+        const { isLoading } = this.state;
+        // received = this.props.received;
+        console.log("is candidate passed? " + candi_date);
 
-        var candi_date = [0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        console.log("candidate: " + candi_date)
         var first = candi_date.indexOf(1)/2;
         var last = candi_date.lastIndexOf(1)/2;
-        
-        var received = [0,0,0,0,0,0,0,0,0,0,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-        var rows = [];
+        //console.log(this.state);
+        //console.log(received);
+        //var received = [0,0,0,0,0,0,0,0,0,0,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+        
+
         for (var i=first; i<last; i++) {
             rows.push(<Time key={i} text={i + ":00"} colour = {"rgb(" + (1-received[i*2])*255 + ",200,230)"}/>)
             rows.push(<Time key={i} text={i + ":30"} colour = {"rgb(" + (1-received[i*2+1])*255 + ",200,230)"}/>)
-            
         }
+        //var candi_date = [0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
+        //console.log("candidate : " + candi_date)
+        console.log("BYE");
 
         return (
             <div className='center' id="main">
             {rows}
 
-                
+
             </div>
         );
     }
--- src/components/TodoListTemplate2.js
+++ src/components/TodoListTemplate2.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import './TodoListTemplate.css';
-import Time from './Time';
+import axios from 'axios';
 import TimeList2 from './TimeList2';
 
+var candi_date_parent = []
+var received_parent = []
+
+class TodoListTemplate2 extends Component {
 
 
-const TodoListTemplate = ({form, children}) => {
-  return (
-    <main className="todo-list-template">
-      <div className="title">
-          전체 스케쥴
-        
-      </div>
-      <div>
-        <TimeList2 className='center'></TimeList2>
-      </div>
 
 
-    </main>
-  );
-};
 
-export default TodoListTemplate;
+  render() {
+    return (
+      <main className="todo-list-template">
+        <div className="title">
+            전체 스케쥴
+          
+        </div>
+        <div>
+          <TimeList2 className='center' candi_date={candi_date_parent} received={received_parent}></TimeList2>
+        </div>
+  
+  
+      </main>
+    );
+
+
+  }
+  
+}
+
+export default TodoListTemplate2;
